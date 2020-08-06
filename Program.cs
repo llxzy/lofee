9ba28b190eaaadbd9813740325b39f595142e02e
llxzy <snowcrash@tuta.io>
2020-08-06 19:09:40 +0200

    Expanded Program.cs, updated gitignore

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 
 namespace lofee
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+        private IConfiguration _configuration;
+
+        private Task Log(LogMessage msg)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            // TODO creating appsettings if it doesnt exist
+            _configuration = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json").Build();
+
+            var botConnectionString = _configuration[$"BotConnectionString"];
+            
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            
+            await _client.LoginAsync(TokenType.Bot, botConnectionString);
+            await _client.StartAsync();
+            
+            //TODO include closeasync to end bot session
+            
+            //Delay Task ending while connection is active
+            await Task.Delay(-1);
+
         }
     }
 }
